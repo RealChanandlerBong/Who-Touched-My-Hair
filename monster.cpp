65dfb1759a91be1bc5ac9723b85c92e34db9fa7d
Dilibra <Dilibraa.outlook.com>
2019-11-09 23:42:31 +0800

    怪兽类改进

--- monster.cpp
+++ monster.cpp
@@ -1,31 +1,29 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
-
-int roadCellAmount=1;//道路格子的总数
-int roadLocation[100][2]={0};//按顺序排列的道路格子位置
-
-=======
->>>>>>> adbb65f20fe4fc1dda7ededc517c517ed8c4cdb7
+/*需要Map提供的变量
+Map.roadAmount; //沿着道路中心线的像素颗粒的总个数（实际上就是怪兽能走的位置）
+int** Map.roadLocation;//按顺序排列的道路中心线颗粒坐标（按顺序排列好的）
+*/
 class Monster{
 public:
+    int roadCellAmount=1;//道路格子的总数
     int intHitPoint;//怪物的血量
     int arrayLocation[2];//怪物的位置
-<<<<<<< HEAD
-	int monsterMoveSpeed;//怪物的速度
-    int locationOrder;//怪物处于第几个道路格子上
+	int monsterMoveSpeed;//怪物的速度(每次刷新走过多少个像素)
+    int locationOrder;//怪物处于第几个道路中心线像素上
     Monster(int blood, int speed){//构造函数
         intHitPoint = blood;
         monsterMoveSpeed = speed;
-        *arrayLocation = *roadLocation[0];
+        *arrayLocation = *Map.roadLocation[0];
     }
-    void move();//怪物前进一次
+    int move();//怪物前进一次，并且返回与终点的道路距离(以像素为单位)
     bool hitHome();//判断怪物是否到老家
 };
 
-void Monster::move(){
+int Monster::move(){
     locationOrder += monsterMoveSpeed;
-    *arrayLocation = *roadLocation[locationOrder];
+    *arrayLocation = *Map.roadLocation[locationOrder];
+    return Map.roadamount - locationOrder;
 }
 
 bool Monster::hitHome(){
@@ -39,6 +37,3 @@ int main(){
     cout<<"1111111";
     return 0;
 }
-=======
-};
->>>>>>> adbb65f20fe4fc1dda7ededc517c517ed8c4cdb7
