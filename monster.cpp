a2989203d643ba8e855abfe272e5a063882fad8c
Dilibra <Dilibraa.outlook.com>
2019-11-29 20:18:08 +0800

    怪兽类更新

--- monster.cpp
+++ monster.cpp
@@ -1,32 +1,25 @@
-#include <iostream>
-using namespace std;
-/*需要Map提供的变量
-Map.roadAmount; //道路格子的总个数
+#include "monster.h"
+/*��ҪMap�ṩ�ı���
+Map.roadAmount; //��·���ӵ��ܸ���
 */
+#include"map.h"
+#include "tower.h"
 
-class Monster{
-public:
-    int intHitPoint;//怪物的血量
-	double monsterMoveSpeed;//怪物的速度(每次刷新走过的距离与一个格子长度的比值)
-    double locationOrder;//怪物的位置，以道路格子为单位，如怪物在第三个格子的1/3位置，则值为2.333333333
-    Monster(int blood, double speed);//构造函数
-    double move();//怪物前进一次，并且返回与终点的距离，以道路格子为单位，同locationOrder
-    bool hitHome();//判断怪物是否到老家
-};
-
-Monster::Monster(int blood, double speed) {
+Monster::Monster(int blood, double speed, Map *thismap) {
     intHitPoint = blood;
     monsterMoveSpeed = speed;
+	map = thismap;
     locationOrder = 0;
 }
 
 double Monster::move() {
     locationOrder += monsterMoveSpeed;
-    return Map.roadAmount - locationOrder;
+    monsterLocation[0] = (map->roadLocation[(int)(locationOrder)][0]) * ((int)(locationOrder) + 1 - locationOrder) + (map->roadLocation[1 + (int)(locationOrder)][0]) * (locationOrder - (int)(locationOrder));
+	monsterLocation[1] = (map->roadLocation[(int)(locationOrder)][1]) * ((int)(locationOrder) + 1 - locationOrder) + (map->roadLocation[1 + (int)(locationOrder)][1]) * (locationOrder - (int)(locationOrder));
+    return (double(map->roadCellAmount) - locationOrder); //�����ô���£�
 }
 
 bool Monster::hitHome() {
-    return (locationOrder >= Map.roadAmount);
+    return (locationOrder >= map->roadCellAmount);
 }
-
 
